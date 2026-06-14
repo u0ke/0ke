f2a92d31fa44d1ed37e479ec722945a22e8e7347
Hamza Bari <barihamza73@gmail.com>
2026-06-14 22:44:10 +0100

    Refactor markCommit function to dynamically generate commit date and update data.json writing logic

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format("YYYY-MM-DD HH:mm:ss");
+const markCommit =  (x, y) => {
+    const date = moment()
+    .subtract(1,"y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format("YYYY-MM-DD HH:mm:ss");
 
 const data = {
     date: date,
 };
 
-jsonfile.writeFile(path, data)
+jsonfile.writeFile(path, data,()=>{
+    simpleGit().add([path]).commit(date, {'--date': date}).push();
+})
     .then(() => console.log("Saved:", data))
     .catch((err) => console.error("Error writing file:", err));
 
-simpleGit().add([path]).commit(date, {'--date': date}).push();
+};
