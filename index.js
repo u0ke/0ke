74b0e81f07456c70a420ab89e8b6d5c818f1cffa
Hamza Bari <barihamza73@gmail.com>
2026-06-14 22:48:04 +0100

    Refactor markCommit function for improved date handling and error logging

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const markCommit =  (x, y) => {
+
+const markCommit = (x, y) => {
     const date = moment()
-    .subtract(1,"y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format("YYYY-MM-DD HH:mm:ss");
+        .subtract(1, "y")
+        .add(1, "d")
+        .subtract(x, "w")
+        .subtract(y, "d")
+        .format("YYYY-MM-DD HH:mm:ss");
 
-const data = {
-    date: date,
+    const data = { date };
+
+    jsonfile.writeFile(path, data)
+        .then(() => {
+            console.log("Saved:", data);
+            return simpleGit()
+                .add([path])
+                .commit(date, { "--date": date })
+                .push();
+        })
+        .catch((err) => console.error("Error:", err));
 };
 
-jsonfile.writeFile(path, data,()=>{
-    simpleGit().add([path]).commit(date, {'--date': date}).push();
-})
-    .then(() => console.log("Saved:", data))
-    .catch((err) => console.error("Error writing file:", err));
-
-};
+// Example: commit as if it were 1 week and 1 day ago
+markCommit(1, 1);
