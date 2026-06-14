61e0b6b2645a597cc9ebb2e0dae4a12f93ad43e2
Hamza Bari <barihamza73@gmail.com>
2026-06-14 22:34:43 +0100

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
+import simpleGit from "simple-git";
 
 const path = "./data.json";
 const date = moment().format("YYYY-MM-DD HH:mm:ss");
@@ -9,3 +10,7 @@ const data = {
 };
 
 jsonfile.writeFile(path, data)
+    .then(() => console.log("Saved:", data))
+    .catch((err) => console.error("Error writing file:", err));
+
+simpleGit().add([path]).commit(date, {'--date': date}).push();
