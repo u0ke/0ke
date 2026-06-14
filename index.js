a14e811f7ad67fd8f7dcd3ad5a107504e8478ecf
Hamza Bari <barihamza73@gmail.com>
2026-06-14 23:15:58 +0100

    Fix commit range in makeCommits function to limit weeks back to 2

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ const markCommit = (x, y) => {
 // 50 commits on random days within roughly the past year, in series.
 const makeCommits = async (n) => {
     for (let i = 0; i < n; i++) {
-        const x = random.int(0, 154); // 0-154 weeks back
+        const x = random.int(0, 2); // 0-64 weeks back
         const y = random.int(0, 6);  // 0-6 days back from that
         await markCommit(x, y);
     }
     console.log(`Done: ${n} commits made.`);
 };
 
-makeCommits(154);
+makeCommits(2);
